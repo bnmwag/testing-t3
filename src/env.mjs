04e37f2c7c0cd6fb7103f30bf22eb7588d262789
bnm.wag <bnm.wag@gmail.com>
2023-05-17 09:59:24 +0200

    env update

--- src/env.mjs
+++ src/env.mjs
@@ -21,8 +21,8 @@ export const env = createEnv({
       process.env.VERCEL ? z.string().min(1) : z.string().url()
     ),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_CLIENT_SECRET: z.string(),
+    DISCORD_CLIENT_ID: z.string().min(1),
+    DISCORD_CLIENT_SECRET: z.string().min(1),
   },
 
   /**
@@ -40,12 +40,6 @@ export const env = createEnv({
    */
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
-    // POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-    // POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-    // POSTGRES_USER: process.env.POSTGRES_USER,
-    // POSTGRES_HOST: process.env.POSTGRES_HOST,
-    // POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-    // POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
